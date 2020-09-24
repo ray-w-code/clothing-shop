0dcd088d450a22f132052102eb60a080b7c8b273
Raymond Wan <rwan615@gmail.com>
2020-09-24 13:51:03 -0700

    add navigation with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Homepage from './pages/homepage/homepage.component';
+import Homepage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
-  return <div><Homepage></Homepage></div>;
-  
+  return (
+    <div>
+      <Switch>
+        <Route path="/" component={Homepage} />
+        
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
